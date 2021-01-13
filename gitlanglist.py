bbf78883bfe320659142fbdcbbd4ef0b924610d6
Bing Yu <bingyu.yap.21@gmail.com>
2021-01-13 21:10:39 +0800

    add gitlanglist and gitscrape

---
+++ gitlanglist.py
@@ -0,0 +1,24 @@
+import bs4 
+from urllib.request import urlopen as uReq 
+from bs4 import BeautifulSoup as soup 
+
+github_url = 'https://github.com'
+my_url = 'https://github.com/trending/python?since=monthly'
+
+uClient = uReq(my_url)
+page_html = uClient.read()
+page_soup = soup(page_html, "html.parser")
+menu = page_soup.find("div", {"id" : "languages-menuitems"})
+
+containers = menu.findAll("a", {"class" : "select-menu-item"})
+
+out_filename = "langList.csv"
+headers = "language, link \n"
+
+f = open(out_filename, "w")
+f.write(headers)
+
+for container in containers:
+    lang = container.find("span" , {"class" : "select-menu-item-text"}).text.strip()
+    lang_url = github_url + container['href']
+    f.write(lang + "," + lang_url + "\n")
