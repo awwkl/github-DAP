af1f1cc1630c638ddefea6634b6ee4ede903ad5e
felicepng <65877521+felicepng@users.noreply.github.com>
2021-01-30 11:12:16 +0800

    Add files via upload

--- clone.py
+++ clone.py
@@ -1,15 +1,18 @@
+import sys
 import os
 import git
 import pandas as pd
 
+# for https
 def clone_repos(lang, max_repos_per_lang=10):
     repo_list = pd.read_csv(f'./repo-namelists/list_{lang}.csv')
 
     for id, repo_name in repo_list['repo_name'].items():
         if id >= max_repos_per_lang:
             break
-        git_url = f'git@github.com:{repo_name}.git'
+        git_url = f'https://github.com/{repo_name}'
         clone_dir = f'./code-repos/{lang}/{id}'
+        clone = f'git clone --quiet {git_url} {clone_dir}'
 
         # if repo has been cloned, do not clone again
         if os.path.exists(clone_dir):
@@ -17,8 +20,8 @@ def clone_repos(lang, max_repos_per_lang=10):
         
         # clone the repo
         print(clone_dir, git_url)
-        git.Repo.clone_from(git_url, clone_dir, depth=1)    # depth=1
+        os.system(clone)
 
-def clone_languages(languages = ['html', 'java', 'python']):
+def clone_languages(languages = ['html', 'java', 'python', 'c', 'c++', 'ruby', 'php']):
     for lang in languages:
         clone_repos(lang)
