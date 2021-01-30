5be7f0353fe775fad60facb021cd37685b7d5813
felicepng <65877521+felicepng@users.noreply.github.com>
2021-01-30 11:31:54 +0800

    Add files via upload

--- clone.py
+++ clone.py
@@ -3,25 +3,42 @@ import os
 import git
 import pandas as pd
 
-# for https
-def clone_repos(lang, max_repos_per_lang=10):
+def clone_repos(option, lang, max_repos_per_lang=5):
     repo_list = pd.read_csv(f'./repo-namelists/list_{lang}.csv')
 
-    for id, repo_name in repo_list['repo_name'].items():
-        if id >= max_repos_per_lang:
-            break
-        git_url = f'https://github.com/{repo_name}'
-        clone_dir = f'./code-repos/{lang}/{id}'
-        clone = f'git clone --quiet {git_url} {clone_dir}'
+    if (option.lower() == "https"):
+        for id, repo_name in repo_list['repo_name'].items():
+            if id >= max_repos_per_lang:
+                break
 
-        # if repo has been cloned, do not clone again
-        if os.path.exists(clone_dir):
-            continue
-        
-        # clone the repo
-        print(clone_dir, git_url)
-        os.system(clone)
+            git_url = f'https://github.com/{repo_name}'
+            clone_dir = f'./code-repos/{lang}/{id}'
+            clone = f'git clone --quiet {git_url} {clone_dir}'
 
-def clone_languages(languages = ['html', 'java', 'python', 'c', 'c++', 'ruby', 'php']):
+            # if repo has been cloned, do not clone again
+            if os.path.exists(clone_dir):
+                continue
+
+            # clone the repo
+            print(clone_dir, git_url)
+            os.system(clone)
+
+    elif (option.lower() == "ssh"):
+        for id, repo_name in repo_list['repo_name'].items():
+            if id >= max_repos_per_lang:
+                break
+
+            git_url = f'git@github.com:{repo_name}.git'
+            clone_dir = f'./code-repos/{lang}/{id}'
+
+            # if repo has been cloned, do not clone again
+            if os.path.exists(clone_dir):
+                continue
+            
+            # clone the repo
+            print(clone_dir, git_url)
+            git.Repo.clone_from(git_url, clone_dir, depth=1)    # depth=1
+
+def clone_languages(option, languages = ['html', 'java', 'python', 'c', 'c++', 'ruby', 'php']):
     for lang in languages:
-        clone_repos(lang)
+        clone_repos(option, lang)
